8d6528f72f66d068d55e17e0ac4279229f1b4d22
c-j-j <chrisjordan1987@gmail.com>
2015-02-19 15:55:52 +0000

    using new game gem

--- app/controllers/tic_tac_toe_controller.rb
+++ app/controllers/tic_tac_toe_controller.rb
@@ -1,24 +1,46 @@
-require 'tictactoe'
+require 'tictactoe/game'
+require 'tictactoe/board_web_presenter'
 
 class TicTacToeController < ApplicationController
 
   INVALID_MOVE_MESSAGE = "Invalid Move, try again"
+  attr_reader :error_message
 
   def home
-    @game_types = TTT::Game::GAME_TYPES
-    @board_sizes = TTT::Game::BOARD_SIZES
+    @game_types = TicTacToe::Game::GAME_TYPES
+    @board_sizes = TicTacToe::Game::BOARD_SIZES
   end
 
   def new_game
-    board = TTT::Board.new(extract_board_size)
+    board = TicTacToe::Board.new(extract_board_size)
     redirect_to play_move_path(play_move_query_params(board))
   end
 
   def play_move
     game = build_game_from_params
     process_turn(game, extract_position_from_param)
+    generate_display_fields(game)
+  end
+
+  def generate_display_fields(game)
     @next_turn_url =  play_move_path(play_move_query_params(game.presenter.board))
+    #TODO this will go when we switch to javascript
+    @computer_has_next_turn = game.current_player_is_computer?
     @game_presenter = game.presenter
+  end
+
+  def process_turn(game, played_position)
+    if game.current_player_is_computer?
+      game.play_turn
+    else
+      submit_user_move(game, played_position)
+    end
+  end
+
+  def submit_user_move(game, played_position)
+    if game.move_valid?(played_position)
+      game.add_move(played_position)
+    end
   end
 
   private
@@ -28,21 +50,13 @@ class TicTacToeController < ApplicationController
   end
 
   def board_param(board)
-    TTT::Web::BoardWebPresenter.to_web_object(board)
-  end
-
-  def process_turn(game, played_position)
-    if game.move_valid?(played_position)
-      game.play_turn(played_position)
-    else
-      @error_message = INVALID_MOVE_MESSAGE
-    end
+    TicTacToe::Web::BoardWebPresenter.to_web_object(board)
   end
 
   def build_game_from_params
     game_type = extract_game_type_from_params
-    board = TTT::Web::BoardWebPresenter.to_board(params['board'])
-    TTT::Game.build_game_with_board(game_type, board)
+    board = TicTacToe::Web::BoardWebPresenter.to_board(params['board'])
+    TicTacToe::Game.build_game_with_board(game_type, board)
   end
 
   def extract_game_type_from_params
--- spec/controllers/tic_tac_toe_controller_spec.rb
+++ spec/controllers/tic_tac_toe_controller_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
-require 'tictactoe'
+require 'tictactoe/board'
+require 'tictactoe/board_web_presenter'
+require 'tictactoe/stubs/stub_game'
 require 'ostruct'
 
 RSpec.describe TicTacToeController, :type => :controller do
-  let(:board) { TTT::Board.new(3) }
-  let(:board_param) { TTT::Web::BoardWebPresenter.to_web_object(board) }
+  let(:game) { TicTacToe::StubGame.new }
+  let(:board) { TicTacToe::Board.new(3) }
+  let(:board_param) { TicTacToe::Web::BoardWebPresenter.to_web_object(board) }
 
   describe 'home' do
     it 'responds successfully with 200 status code' do
@@ -19,26 +22,28 @@ RSpec.describe TicTacToeController, :type => :controller do
 
     it 'loads game options into game_types' do
       get(:home)
-      expect(assigns(:game_types)).to match_array(TTT::Game::GAME_TYPES)
+      expect(assigns(:game_types)).to match_array(TicTacToe::Game::GAME_TYPES)
     end
 
     it 'loads board sizes into board_sizes' do
       get(:home)
-      expect(assigns(:board_sizes)).to match_array(TTT::Game::BOARD_SIZES)
+      expect(assigns(:board_sizes)).to match_array(TicTacToe::Game::BOARD_SIZES)
     end
   end
 
   describe 'new_game' do
     it 'redirects to play_move with board and game type' do
-      get(:new_game, {'game_type' => TTT::Game::HVH, 'board_size' => '3'} )
+      get(:new_game, {'game_type' => TicTacToe::Game::HVH, 'board_size' => '3'} )
 
       expect(response).to redirect_to(play_move_path(
-        'game_type' => TTT::Game::HVH,
+        'game_type' => TicTacToe::Game::HVH,
         'board' => board_param ))
     end
   end
 
   describe 'play_move' do
+    let(:controller) { TicTacToeController.new }
+
     it 'renders play_move template' do
       get_play_move
       expect(response).to render_template('play_move')
@@ -51,19 +56,29 @@ RSpec.describe TicTacToeController, :type => :controller do
 
     it 'builds url for next turn' do
       get_play_move
-      expect(assigns(:next_turn_url)).to eq(play_move_path(
-        'game_type' => TTT::Game::HVH,
-        'board' => board_param ))
+      expect(assigns(:next_turn_url)).to include('board')
+      expect(assigns(:next_turn_url)).to include('game_type')
     end
 
-    it 'sets error message when invalid move given' do
-      get(:play_move, {'board' => board_param, 'game_type' => TTT::Game::HVH,
-      'position' => '-1'})
-      expect(assigns(:error_message)).to eq(TicTacToeController::INVALID_MOVE_MESSAGE)
+    it 'plays turn on game when current player is computer' do
+      game.set_current_player_to_computer(true)
+      controller.process_turn(game, nil)
+      expect(game.play_turn_called?).to eq(true)
+    end
+
+    it 'adds move to game when human player has sent their move' do
+      game.set_current_player_to_computer(false)
+      controller.process_turn(game, 'user move')
+      expect(game.add_move_called?).to eq(true)
+    end
+
+    it 'returns success' do
+      get(:play_move, {'position' => '1', 'board' => board_param,  'game_type' => TicTacToe::Game::HVH} )
+      expect(response).to be_success
     end
 
     def get_play_move
-      get(:play_move, {'board' => board_param, 'game_type' => TTT::Game::HVH })
+      get(:play_move, {'board' => board_param, 'game_type' => TicTacToe::Game::HVH })
     end
 
   end
